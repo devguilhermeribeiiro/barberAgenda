f44dce2601c7498cdde7bfe4bf16055a5e4c6202
devgr <devguilhermeribeiro000@gmail.com>
2025-05-28 23:37:03 -0300

    Implementing the data access object

--- src/Dao/ScheduleDao.php
+++ src/Dao/ScheduleDao.php
@@ -2,6 +2,72 @@
 
 namespace BarberAgenda\Dao;
 
+use BarberAgenda\Dto\ScheduleResponseDto;
+use BarberAgenda\Entity\Schedule;
+use PDO;
+
 class ScheduleDao
 {
+    private PDO $conn;
+
+    public function __construct(PDO $conn) {
+        $this->conn = $conn;
+    }
+
+    public function findAll(): array
+    {
+        $rs = $this->conn->query("SELECT * FROM schedules");
+        $schedules = $rs->fetch(PDO::FETCH_ASSOC);
+        return $schedules;   
+    }
+
+    public function findById(int $id): ScheduleResponseDto {
+        $stmt = $this->conn->prepare("SELECT * FROM schedules WHERE id = ?");
+        $stmt->bindParam(1, $id);
+        $rs = $stmt->execute();
+
+        return new ScheduleResponseDto;
+    }
+
+    public function save(Schedule $schedule): ScheduleResponseDto {
+        $stmt = $this->conn->prepare("INSERT INTO schedules (service, barber, date, hour) VALUES (?, ?, ?) RETURNING *");
+
+        $stmt->bindParam(
+            1, $schedule->getService(),
+            2, $schedule->getBarber(),
+            3, $schedule->getDate(),
+            4, $schedule->getHour(),
+        );
+
+        $stmt->execute();
+        $rs = $stmt->fetchObject("ScheduleResponseDto");
+
+        return $rs;
+    }
+
+    public function update(Schedule $schedule): ScheduleResponseDto {
+        $stmt = $this->conn->prepare("UPDATE schedules SET service = ?, barber = ?, date = ?, hour = ? WHERE id = ? RETURNING *");
+        
+        $stmt->bindParam(
+            1, $schedule->getService(),
+            2, $schedule->getBarber(),
+            3, $schedule->getDate(),
+            4, $schedule->getHour(),
+        );
+
+        $stmt->execute();
+        $rs = $stmt->fetchObject("ScheduleResponseDto");
+
+        return $rs;
+    }
+
+    public function destroy(int $id): ScheduleResponseDto {
+        $stmt = $this->conn->prepare("DELETE FROM schedules WHERE id = ? RETURNING *");
+
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+        $rs = $stmt->fetchObject("ScheduleResponseDto");
+
+        return $rs;
+    }
 }
