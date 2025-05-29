258cd213c83dfebe777e35818779fec37435a3bf
devgr <devguilhermeribeiro000@gmail.com>
2025-05-28 23:37:29 -0300

    Implementing entity

--- src/Entity/Schedule.php
+++ src/Entity/Schedule.php
@@ -2,4 +2,33 @@
 
 namespace BarberAgenda\Entity;
 
-class Schedule {}
+class Schedule {
+    private string $service;
+    private string $barber;
+    private string $date;
+    private string $hour;
+
+    public function __construct(string $service, string $barber, string $date, string $hour) {
+        $this->service = $service;
+        $this->barber = $barber;
+        $this->date = $date;
+        $this->hour = $hour;
+    }
+
+    public function getService(): string {
+        return $this->service;
+    }
+
+    public function getBarber(): string {
+        return $this->barber;
+    }
+
+    public function getdate() : string {
+        return $this->date;
+    }
+
+    public function getHour(): string {
+        return $this->hour;
+    }
+
+}
