9e0a82c9d222c775da0571a7471fb065a6bcf08c
devgr <devguilhermeribeiro000@gmail.com>
2025-05-30 14:51:11 -0300

    Creating a custom Route type

---
+++ src/Utils/Types/Route.php
@@ -0,0 +1,17 @@
+<?php
+
+class Route {
+    private string $method;
+    private string $path;
+    private callable $callback;
+
+    public function __construct(string $method, string $path, callable $callback) {
+        $this->method = $method;
+        $this->path = $path;
+        $this->callback = $callback;
+    }
+
+    public function method() { return $this->method; }
+    public function path() { return $this->path; }
+    public function callback() { return $this->callback; }
+}
