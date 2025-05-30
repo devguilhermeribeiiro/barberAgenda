cd444c55fc68b055e0d0fc7bdeae5a4f1022ba75
devgr <devguilhermeribeiro000@gmail.com>
2025-05-29 23:14:52 -0300

    Get injected dependencies

--- src/Config/DependencyContainer.php
+++ src/Config/DependencyContainer.php
@@ -2,6 +2,17 @@
 
 namespace BarberAgenda\Config;
 
+use BarberAgenda\Controllers\ScheduleController;
+use BarberAgenda\Repository\ScheduleRepository;
+use BarberAgenda\Services\ScheduleService;
+
 class DependencyContainer
 {
+    public static function inject(): ScheduleController {
+        $repository = new ScheduleRepository;
+        $service = new ScheduleService($repository);
+        $controller = new ScheduleController($service);
+
+        return $controller;
+    }
 }
