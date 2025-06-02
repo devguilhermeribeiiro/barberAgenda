d7627e13735482bb29d27cd3de503397ff9f4db9
devgr <devguilhermeribeiro000@gmail.com>
2025-06-02 14:34:49 -0300

    Calling router statically

--- src/Bootstrap/App.php
+++ src/Bootstrap/App.php
@@ -3,22 +3,21 @@
 namespace BarberAgenda\Bootstrap;
 
 use BarberAgenda\Utils\Types\Route;
-use Router;
+use BarberAgenda\Config\Router;
 
 class App {
-    private Router $router = new Router;
-
+    
     public function run(): void {
         $this->defineRoutes();
-        $this->router->handle($_SERVER["REQUEST_METHOD"], [
+        Router::handle($_SERVER["REQUEST_METHOD"], [
             "path" => parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH),
-            "query" => explode('=', parse_url($_SERVER["REQUEST_URI"], PHP_URL_QUERY))
+            "query" => explode("=", parse_url($_SERVER["REQUEST_URI"], PHP_URL_QUERY) ?? '')
         ]);
     }
 
     public function defineRoutes() : void {
         $routes = [
-            new Route('GET', '/', function () { echo "<h1>Hello, World!</h1>"; }),
+            new Route('GET', '/', function () { echo json_encode(["message" => "Hello, world!"]); }),
             new Route('GET', '/schedules', 'ScheduleController@index'),
             new Route('POST', '/schedules', 'ScheduleController@create'),
             new Route('GET', '/schedule', 'ScheduleController@show'),
@@ -27,7 +26,7 @@ class App {
         ];
 
         foreach ($routes as $route) {
-            $this->router->add($route);
+            Router::add($route);
         }
     }
 }
