0b06218cc5df3df8c5ba95efa9f341eec5ef4809
devgr <devguilhermeribeiro000@gmail.com>
2025-06-05 14:46:15 -0300

    Refactoring router to use regex and call verb named static methods to add a route

--- src/Bootstrap/App.php
+++ src/Bootstrap/App.php
@@ -9,24 +9,12 @@ class App {
     
     public function run(): void {
         $this->defineRoutes();
-        Router::handle($_SERVER["REQUEST_METHOD"], [
-            "path" => parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH),
-            "query" => explode("=", parse_url($_SERVER["REQUEST_URI"], PHP_URL_QUERY) ?? '')
-        ]);
+        Router::handle();
     }
 
     public function defineRoutes() : void {
-        $routes = [
-            new Route('GET', '/', function () { echo json_encode(["message" => "Hello, world!"]); }),
-            new Route('GET', '/schedules', 'ScheduleController@index'),
-            new Route('POST', '/schedules', 'ScheduleController@create'),
-            new Route('GET', '/schedule', 'ScheduleController@show'),
-            new Route('PUT', '/schedule', 'ScheduleController@update'),
-            new Route('DELETE', '/schedule', 'ScheduleController@destroy'),
-        ];
-
-        foreach ($routes as $route) {
-            Router::add($route);
-        }
+        Router::get('/{name}', function ($name) {
+            echo json_encode(["message" => "Hello, $name."]);
+        });
     }
 }
--- src/Config/Router.php
+++ src/Config/Router.php
@@ -2,42 +2,71 @@
 
 namespace BarberAgenda\Config;
 
-use BarberAgenda\Utils\Types\Route;
-
 class Router {
-    private static array $routes = [];
+    private static $routes;
 
-    public static function add(Route $route): void {
-        array_push(self::$routes, $route);
+    public static function add($method, $path, $callback): void {
+        self::$routes[] = [
+            "method" => $method,
+            "path" => $path,
+            "callback" => $callback,
+        ];
     }
 
-    public static function handle(string $requestMethod, array $requestUri) {
+    public static function handle() {
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $requestUri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
         foreach (self::$routes as $route) {
-            if (!$requestMethod == strtoupper($route->method()) && $requestUri["path"] == $route->path()) {
-                http_response_code(404);
-                echo json_encode(["error" => "route not found."]);
+            $paramsPattern = '#^' . preg_replace('/\{([\w]+)\}/', '([^/]+)', $route["path"]) . '$#';
+            $url_params = [];
+
+            if ($requestMethod === $route["method"] && preg_match($paramsPattern, $requestUri, $url_params)) {
+                array_shift($url_params);
+
             }
 
-            if (!is_callable($route->callback())) {
-                if (!str_contains("@", $route->callback())) {
-                    echo json_encode(["error" => "invalid callback"]);
-                }
-                
-                [$controllerName, $action] = explode("@", $route->callback());
+            if (is_callable($route["callback"])) {
+                return call_user_func_array($route["callback"], $url_params);
+            } elseif (str_contains($route["callback"], "@")) {
+                [$controllerName, $action] = explode("@", $route["callback"]);
 
                 $controller = "\\BarberAgenda\\Controllers\\" . $controllerName;
 
-                if (!class_exists($controller) && method_exists($controller, $action)) {
-                    http_response_code(400);
+                if (!class_exists($controller)) {
                     echo json_encode(["error" => "invalid callback"]);
+                    return;
                 }
+
                 $dependencies = new DependencyContainer();
                 $controller = new $controller($dependencies->inject());
 
-                call_user_func_array([$controller, $action], $requestUri["query"]);
+                return call_user_func_array([$controller, $action], $url_params);
             }
-
-            call_user_func($route->callback(), array_pop($requestUri["query"]));
         }
+
+        http_response_code(404);
+        echo json_encode("Not found");
     }
+
+    public static function get($path, $callback)
+    {
+        self::add('GET', $path, $callback);
+    }
+
+    public static function post($path, $callback)
+    {
+        self::add('POST', $path, $callback);
+    }
+
+    public static function put($path, $callback)
+    {
+        self::add('PUT', $path, $callback);
+    }
+
+    public static function delete($path, $callback)
+    {
+        self::add('DELETE', $path, $callback);
+    }
+
 }
