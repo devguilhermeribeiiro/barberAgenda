a88cab38f5fdd69e732d879bcc37759d8fbb0cf2
devgr <devguilhermeribeiro000@gmail.com>
2025-06-10 23:38:06 -0300

    Defining crud routes

--- src/Bootstrap/App.php
+++ src/Bootstrap/App.php
@@ -12,8 +12,18 @@ class App {
     }
 
     public function defineRoutes() : void {
-        Router::get('/{name}', function ($name) {
+        Router::get('/name/{name}', function ($name) {
             echo json_encode(["message" => "Hello, $name."]);
         });
+        
+        Router::get('/schedules', 'ScheduleController@index');
+
+        Router::post('/schedules', 'ScheduleController@create');
+
+        Router::get('/schedules/{id}', 'ScheduleController@show');
+
+        Router::post('/schedules/{id}', 'ScheduleController@update');
+
+        Router::post('/schedules/{id}/destroy', 'ScheduleController@destroy');
     }
 }
